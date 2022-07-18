a5f0285f4029afb9318c500ec55f193c2bd12edb
William Baker <bbrk24@gmail.com>
2022-07-18 13:13:46 -0400

    Add ruby item

--- src/main/java/org/bbrk24/amurians/Initializer.java
+++ src/main/java/org/bbrk24/amurians/Initializer.java
@@ -1,10 +1,22 @@
 package org.bbrk24.amurians;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Initializer implements ModInitializer {
+    private static final Item RUBY = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+
     @Override
 	public void onInitialize() {
-        // initialization code here
+        Registry.register(
+            Registry.ITEM,
+            new Identifier("amurians", "ruby"),
+            RUBY
+        );
     }
 }
