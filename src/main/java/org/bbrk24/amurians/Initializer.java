e3ca7b0fdbee2a418df841a9831631269a681225
William Baker <bbrk24@gmail.com>
2022-07-18 13:33:50 -0400

    Add ruby block and correct JSON style

--- src/main/java/org/bbrk24/amurians/Initializer.java
+++ src/main/java/org/bbrk24/amurians/Initializer.java
@@ -2,7 +2,10 @@ package org.bbrk24.amurians;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -11,8 +14,26 @@ import net.minecraft.util.registry.Registry;
 public class Initializer implements ModInitializer {
     private static final Item RUBY = new Item(new FabricItemSettings().group(ItemGroup.MISC));
 
+    private static final Block RUBY_BLOCK = new Block(
+        FabricBlockSettings.of(Material.METAL)
+            .strength(5.0f, 6.0f)
+            .requiresTool()
+    );
+
     @Override
 	public void onInitialize() {
+        Registry.register(
+            Registry.BLOCK,
+            new Identifier("amurians", "ruby_block"),
+            RUBY_BLOCK
+        );
+
+        Registry.register(
+            Registry.ITEM,
+            new Identifier("amurians", "ruby_block"),
+            new BlockItem(RUBY_BLOCK, new FabricItemSettings().group(ItemGroup.MISC))
+        );
+
         Registry.register(
             Registry.ITEM,
             new Identifier("amurians", "ruby"),
