dcd245f6ef18836eb4add6773c74b94a682627d1
William Baker <bbrk24@gmail.com>
2022-07-18 12:43:08 -0400

    Scaffold mod

---
+++ src/main/java/org/bbrk24/amurians/Initializer.java
@@ -0,0 +1,10 @@
+package org.bbrk24.amurians;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Initializer implements ModInitializer {
+    @Override
+	public void onInitialize() {
+        // initialization code here
+    }
+}
