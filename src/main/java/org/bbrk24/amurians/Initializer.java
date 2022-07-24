7cabac290fcbc014256623123b48f92d5ae76fb7
William Baker <bbrk24@gmail.com>
2022-07-24 00:33:18 -0400

    Add basic azalea wood blocks

--- src/main/java/org/bbrk24/amurians/Initializer.java
+++ src/main/java/org/bbrk24/amurians/Initializer.java
@@ -11,6 +11,7 @@ import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.Material;
+import net.minecraft.block.PillarBlock;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
@@ -23,6 +24,7 @@ import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.RecipeType;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
 
 public class Initializer implements ModInitializer {
@@ -52,12 +54,36 @@ public class Initializer implements ModInitializer {
     );
 
     public static final Block RUBY_BLOCK = new Block(
-        FabricBlockSettings.of(Material.METAL)
+        FabricBlockSettings.of(Material.METAL, MapColor.BRIGHT_RED)
             .strength(5.0f, 6.0f)
             .requiresTool()
-            .mapColor(MapColor.BRIGHT_RED)
     );
     public static final Block EMERY_TABLE = new EmeryTableBlock();
+    public static final Block AZALEA_LOG = new PillarBlock(
+        FabricBlockSettings.of(
+            Material.WOOD,
+            state -> state.get(PillarBlock.AXIS) == Direction.Axis.Y
+                ? MapColor.PALE_YELLOW
+                : MapColor.SPRUCE_BROWN
+        )
+            .strength(2.0f)
+    );
+    public static final Block AZALEA_PLANKS = new Block(
+        FabricBlockSettings.of(Material.WOOD, MapColor.PALE_YELLOW)
+            .strength(2.0f)
+    );
+    public static final Block STRIPPED_AZALEA_LOG = new PillarBlock(
+        FabricBlockSettings.of(Material.WOOD, MapColor.PALE_YELLOW)
+            .strength(2.0f)
+    );
+    public static final Block AZALEA_WOOD = new PillarBlock(
+        FabricBlockSettings.of(Material.WOOD, MapColor.SPRUCE_BROWN)
+            .strength(2.0f)
+    );
+    public static final Block STRIPPED_AZALEA_WOOD = new PillarBlock(
+        FabricBlockSettings.of(Material.WOOD, MapColor.PALE_YELLOW)
+            .strength(2.0f)
+    );
 
     public static final EntityType<AmurianEntity> AMURIAN = Registry.register(
         Registry.ENTITY_TYPE,
@@ -86,6 +112,31 @@ public class Initializer implements ModInitializer {
             new Identifier("amurians", "emery_table"),
             EMERY_TABLE
         );
+        Registry.register(
+            Registry.BLOCK,
+            new Identifier("amurians", "azalea_log"),
+            AZALEA_LOG
+        );
+        Registry.register(
+            Registry.BLOCK,
+            new Identifier("amurians", "azalea_planks"),
+            AZALEA_PLANKS
+        );
+        Registry.register(
+            Registry.BLOCK,
+            new Identifier("amurians", "stripped_azalea_log"),
+            STRIPPED_AZALEA_LOG
+        );
+        Registry.register(
+            Registry.BLOCK,
+            new Identifier("amurians", "azalea_wood"),
+            AZALEA_WOOD
+        );
+        Registry.register(
+            Registry.BLOCK,
+            new Identifier("amurians", "stripped_azalea_wood"),
+            STRIPPED_AZALEA_WOOD
+        );
 
         Registry.register(
             Registry.ITEM,
@@ -96,6 +147,31 @@ public class Initializer implements ModInitializer {
             Registry.ITEM,
             new Identifier("amurians", "emery_table"),
             new BlockItem(EMERY_TABLE, new FabricItemSettings().group(ItemGroup.DECORATIONS))
+        );
+        Registry.register(
+            Registry.ITEM,
+            new Identifier("amurians", "azalea_log"),
+            new BlockItem(AZALEA_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS))
+        );
+        Registry.register(
+            Registry.ITEM,
+            new Identifier("amurians", "azalea_planks"),
+            new BlockItem(AZALEA_PLANKS, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS))
+        );
+        Registry.register(
+            Registry.ITEM,
+            new Identifier("amurians", "stripped_azalea_log"),
+            new BlockItem(STRIPPED_AZALEA_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS))
+        );
+        Registry.register(
+            Registry.ITEM,
+            new Identifier("amurians", "azalea_wood"),
+            new BlockItem(AZALEA_WOOD, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS))
+        );
+         Registry.register(
+            Registry.ITEM,
+            new Identifier("amurians", "stripped_azalea_wood"),
+            new BlockItem(STRIPPED_AZALEA_WOOD, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS))
         );
 
         Registry.register(
