10202766f39e33ad61f9da7fbc93b6f37b1a1cf4
William Baker <bbrk24@gmail.com>
2022-07-30 10:46:12 -0400

    Kotlin: Y U NO have public static fields? :(

---
+++ src/main/kotlin/org/bbrk24/amurians/Client.kt
@@ -0,0 +1,27 @@
+package org.bbrk24.amurians
+
+import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry
+import net.minecraft.client.gui.screen.ingame.HandledScreens
+import net.minecraft.client.model.TexturedModelData
+import net.minecraft.client.render.entity.model.EntityModelLayer
+import net.minecraft.client.render.entity.model.VillagerResemblingModel
+import net.minecraft.util.Identifier
+
+val AMURIAN_MODEL_LAYER = EntityModelLayer(Identifier("amurians", "amurian"), "main")
+
+@Environment(EnvType.CLIENT)
+class Client : ClientModInitializer {
+    override fun onInitializeClient() {
+        EntityRendererRegistry.register(Initializer.AMURIAN, ::AmurianRenderer)
+        EntityModelLayerRegistry.registerModelLayer(
+            AMURIAN_MODEL_LAYER,
+            { -> TexturedModelData.of(VillagerResemblingModel.getModelData(), 64, 64) }
+        )
+
+        HandledScreens.register(Initializer.EMERY_TABLE_SCREEN_HANDLER_TYPE, ::EmeryTableScreen)
+    }
+}
