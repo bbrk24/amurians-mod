4904147bdece376fec88c5404848c19136bb3e33
William Baker <bbrk24@gmail.com>
2022-07-30 11:15:06 -0400

    Probably the last thing I'm making Kotlin for now

---
+++ src/main/kotlin/org/bbrk24/amurians/EmeryTableRecipeSerializer.kt
@@ -0,0 +1,47 @@
+package org.bbrk24.amurians
+
+import com.google.gson.JsonObject
+
+import net.minecraft.item.ItemStack
+import net.minecraft.network.PacketByteBuf
+import net.minecraft.recipe.Ingredient
+import net.minecraft.recipe.RecipeSerializer
+import net.minecraft.util.Identifier
+import net.minecraft.util.JsonHelper
+import net.minecraft.util.registry.Registry
+
+class EmeryTableRecipeSerializer : RecipeSerializer<EmeryTableRecipe> {
+    override fun read(id: Identifier, json: JsonObject): EmeryTableRecipe {
+        // Kotlin can't parse the @Contract annotation, but this will never return null if the third
+        // argument is non-null.
+        val group = JsonHelper.getString(json, "group", "")!!
+
+        val ingredient = Ingredient.fromJson(
+            if (JsonHelper.hasArray(json, "ingredient"))
+                JsonHelper.getArray(json, "ingredient")
+            else
+                JsonHelper.getObject(json, "ingredient")
+        )
+        val resultId = Identifier(JsonHelper.getString(json, "result"))
+        val resultCount = JsonHelper.getInt(json, "count", 1)
+        return EmeryTableRecipe(
+            id,
+            group,
+            ingredient,
+            ItemStack(Registry.ITEM.get(resultId), resultCount)
+        )
+    }
+
+    override fun read(id: Identifier, buf: PacketByteBuf): EmeryTableRecipe {
+        val group = buf.readString()
+        val ingredient = Ingredient.fromPacket(buf)
+        val output = buf.readItemStack()
+        return EmeryTableRecipe(id, group, ingredient, output)
+    }
+
+    override fun write(buf: PacketByteBuf, recipe: EmeryTableRecipe) {
+        buf.writeString(recipe.getGroup())
+        recipe.getInput().write(buf)
+        buf.writeItemStack(recipe.getOutput())
+    }
+}
