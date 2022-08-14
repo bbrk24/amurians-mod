752eeab5846e3d5d1e6b8464f8b39fa6e2280861
William Baker <bbrk24@gmail.com>
2022-08-13 22:36:21 -0400

    break together

--- src/main/kotlin/org/bbrk24/amurians/hishai/HishaiBlock.kt
+++ src/main/kotlin/org/bbrk24/amurians/hishai/HishaiBlock.kt
@@ -3,6 +3,7 @@ package org.bbrk24.amurians.hishai
 import kotlin.math.min
 
 import net.minecraft.block.Block
+import net.minecraft.block.Blocks
 import net.minecraft.block.BlockState
 import net.minecraft.block.Fertilizable
 import net.minecraft.block.PlantBlock
@@ -19,12 +20,15 @@ import net.minecraft.util.Hand
 import net.minecraft.util.Identifier
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.util.math.random.Random
 import net.minecraft.util.registry.Registry
 import net.minecraft.util.shape.VoxelShape
 import net.minecraft.util.shape.VoxelShapes
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
+import net.minecraft.world.WorldAccess
+import net.minecraft.world.WorldEvents
 import net.minecraft.world.event.GameEvent
 
 import org.bbrk24.amurians.Initializer
@@ -35,6 +39,8 @@ internal val AGE = Properties.AGE_15
 internal const val MAX_AGE = 15
 internal const val MEDIUM_CUTOFF_AGE = 4
 internal const val LARGE_CUTOFF_AGE = 10
+
+internal const val MIN_LIGHT_LEVEL = 9
 
 private val SMALL_OUTLINE = Block.createCuboidShape(5.0, 0.0, 5.0, 11.0, 8.0, 11.0)
 private val MEDIUM_OUTLINE = Block.createCuboidShape(2.0, 0.0, 2.0, 14.0, 15.0, 14.0)
@@ -71,7 +77,7 @@ class HishaiBlock(settings: Settings) : PlantBlock(settings), Fertilizable {
         val aboveState = world.getBlockState(abovePos)
 
         return (aboveState.isAir() || aboveState.isOf(Initializer.HISHAI_TOP)) &&
-            world.getBaseLightLevel(abovePos, 0) >= 9
+            world.getBaseLightLevel(abovePos, 0) >= MIN_LIGHT_LEVEL
     }
 
     private fun growBy(state: BlockState, world: ServerWorld, pos: BlockPos, amount: Int) {
@@ -98,7 +104,7 @@ class HishaiBlock(settings: Settings) : PlantBlock(settings), Fertilizable {
         pos: BlockPos,
         state: BlockState,
         isClient: Boolean
-    ) = true
+    ) = state.get(AGE) < MAX_AGE
 
     override fun canGrow(world: World, random: Random, pos: BlockPos, state: BlockState): Boolean {
         return shouldGrow(state, pos, world)
@@ -188,4 +194,47 @@ class HishaiBlock(settings: Settings) : PlantBlock(settings), Fertilizable {
 
     // c'mon, let me use transparent pixels
     override fun isTranslucent(state: BlockState, world: BlockView, pos: BlockPos) = true
+
+    override fun onBreak(world: World, pos: BlockPos, state: BlockState, player: PlayerEntity) {
+        if (state.get(AGE) >= LARGE_CUTOFF_AGE) {
+            val abovePos = pos.up()
+            val aboveState = world.getBlockState(abovePos)
+            world.setBlockState(abovePos, Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL)
+            world.syncWorldEvent(player, WorldEvents.BLOCK_BROKEN, abovePos, Block.getRawIdFromState(aboveState))
+        }
+        super.onBreak(world, pos, state, player)
+    }
+
+    override fun scheduledTick(state: BlockState, world: ServerWorld, pos: BlockPos, random: Random) {
+        if (
+            state.get(AGE) >= LARGE_CUTOFF_AGE &&
+                !world.getBlockState(pos.up()).isOf(Initializer.HISHAI_TOP)
+        ) {
+            world.breakBlock(pos, true)
+        }
+    }
+
+    override fun getStateForNeighborUpdate(
+        state: BlockState,
+        direction: Direction,
+        neighborState: BlockState,
+        world: WorldAccess,
+        pos: BlockPos,
+        neighborPos: BlockPos
+    ): BlockState {
+        if (
+            state.get(AGE) >= LARGE_CUTOFF_AGE &&
+                !world.getBlockState(pos.up()).isOf(Initializer.HISHAI_TOP)
+        ) {
+            world.createAndScheduleBlockTick(pos, this, 1)
+        }
+        return super.getStateForNeighborUpdate(
+            state,
+            direction,
+            neighborState,
+            world,
+            pos,
+            neighborPos
+        )
+    }
 }
--- src/main/kotlin/org/bbrk24/amurians/hishai/HishaiTopBlock.kt
+++ src/main/kotlin/org/bbrk24/amurians/hishai/HishaiTopBlock.kt
@@ -1,6 +1,7 @@
 package org.bbrk24.amurians.hishai
 
 import net.minecraft.block.Block
+import net.minecraft.block.Blocks
 import net.minecraft.block.BlockState
 import net.minecraft.block.Fertilizable
 import net.minecraft.block.ShapeContext
@@ -14,9 +15,12 @@ import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.util.math.random.Random
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
+import net.minecraft.world.WorldAccess
+import net.minecraft.world.WorldEvents
 import net.minecraft.world.event.GameEvent
 
 import org.bbrk24.amurians.Initializer
@@ -40,10 +44,10 @@ class HishaiTopBlock(settings: Settings) : Block(settings), Fertilizable {
         pos: BlockPos,
         state: BlockState,
         isClient: Boolean
-    ) = true
+    ) = !state.get(Properties.BERRIES)
 
     override fun canGrow(world: World, random: Random, pos: BlockPos, state: BlockState): Boolean {
-        return !state.get(Properties.BERRIES)
+        return !state.get(Properties.BERRIES) && world.getBaseLightLevel(pos, 0) >= MIN_LIGHT_LEVEL
     }
 
     override fun grow(world: ServerWorld, random: Random, pos: BlockPos, state: BlockState) {
@@ -89,5 +93,39 @@ class HishaiTopBlock(settings: Settings) : Block(settings), Fertilizable {
 
     override fun getPickStack(world: BlockView, pos: BlockPos, state: BlockState): ItemStack {
         return ItemStack(Initializer.HISHAI_PLANT)
+    }
+
+    override fun onBreak(world: World, pos: BlockPos, state: BlockState, player: PlayerEntity) {
+        world.breakBlock(pos.down(), !player.isCreative())
+        super.onBreak(world, pos, state, player)
+    }
+
+    override fun scheduledTick(state: BlockState, world: ServerWorld, pos: BlockPos, random: Random) {
+        val belowState = world.getBlockState(pos.down())
+        if (!belowState.isOf(Initializer.HISHAI_PLANT) || belowState.get(AGE) < LARGE_CUTOFF_AGE) {
+            world.breakBlock(pos, false)
+        }
+    }
+
+    override fun getStateForNeighborUpdate(
+        state: BlockState,
+        direction: Direction,
+        neighborState: BlockState,
+        world: WorldAccess,
+        pos: BlockPos,
+        neighborPos: BlockPos
+    ): BlockState {
+        val belowState = world.getBlockState(pos.down())
+        if (!belowState.isOf(Initializer.HISHAI_PLANT) || belowState.get(AGE) < LARGE_CUTOFF_AGE) {
+            world.createAndScheduleBlockTick(pos, this, 1)
+        }
+        return super.getStateForNeighborUpdate(
+            state,
+            direction,
+            neighborState,
+            world,
+            pos,
+            neighborPos
+        )
     }
 }
